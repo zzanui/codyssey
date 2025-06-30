5f0dc061c103998c3989779d266caa542cc5661d
zzanui <103357002+zzanui@users.noreply.github.com>
2025-06-30 10:07:25 +0000

    2-2 finish

--- chepter_1/question2/main.py
+++ chepter_1/question2/main.py
@@ -21,10 +21,9 @@ with open('chepter_1/question2/mission_computer_main.log', 'r', encoding='utf8' 
 
 # 전환된 리스트 객체를 화면에 출력      
 ######################################################
-
-# print("Timestamp:", timestamp)
-# print("Event:", event)
-# print("Message:", message)
+print("Timestamp:", timestamp)
+print("Event:", event)
+print("Message:", message)
 
 
 
@@ -46,9 +45,12 @@ message = list(message)
 
 #리스트 객체를 사전(Dict) 객체로 전환
 ######################################################
-#사전 객체로 변환
-timestamp_dic = {timestamp[i]: {'event': event[i], 'message': message[i]} for i in range(len(timestamp))}
+#사전 객체로 변환하는데 하나하나 넣어주는 과정을 리스트의 길이만큼 반복한다.
+loc_dic = {timestamp[i]: {'event': event[i], 'message': message[i]} for i in range(len(timestamp))}
+
 
 #사전 객체로 전환된 내용을 mission_computer_main.json 파일로 저장하는데 파일 포멧은 JSON(JavaScript Ontation)으로 저장한다.
+######################################################
 file_path = 'mission_computer_main.json'
-json
+with open(file_path,'w', encoding='UTF-8') as f:
+  json.dump(loc_dic, f)
